a4462525171e2952af87881d29d74906ca9420e2
Kodaira Takumi <a48565493@icloud.com>
2022-12-26 21:50:50 +0900

    if bunn talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,19 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.msg import Query
+
+def cb(request, response):
+    if request.name = "小平拓海":
+        response.age = 20
+    else:
+        response.age = 44
+
+    return response
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person, "person", 10)
-n = 0
-
-def cb():
-    global n
-    msg = Person()
-    msg.name = "小平拓海"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
-
 node.create_timer(0.5, cb)
 rclpy.spin(node)
